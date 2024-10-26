f00aba2c46420b5d641438b5d884b181104f2130
Olumide Awodeji <47766490+Cypher-O@users.noreply.github.com>
2024-10-26 09:37:52 +0100

    minor change

--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -13,22 +13,22 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/", response_model=schemas.Recipe)
+@router.post("/recipes/", response_model=schemas.Recipe)
 def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
     return recipe_controller.create_recipe(db, recipe)
 
-@router.get("/", response_model=list[schemas.Recipe])
+@router.get("/recipes/", response_model=list[schemas.Recipe])
 def read_recipes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return recipe_controller.read_recipes(db, skip=skip, limit=limit)
 
-@router.get("/{recipe_id}", response_model=schemas.Recipe)
+@router.get("/recipes/{recipe_id}", response_model=schemas.Recipe)
 def read_recipe(recipe_id: int, db: Session = Depends(get_db)):
     return recipe_controller.read_recipe(db, recipe_id)
 
-@router.put("/{recipe_id}", response_model=schemas.Recipe)
+@router.put("/recipes/{recipe_id}", response_model=schemas.Recipe)
 def update_recipe(recipe_id: int, recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
     return recipe_controller.update_recipe(db, recipe_id, recipe)
 
-@router.delete("/{recipe_id}")
+@router.delete("/recipes/{recipe_id}")
 def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
     return recipe_controller.delete_recipe(db, recipe_id)
